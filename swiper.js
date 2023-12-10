228c59ba0d15543ec7ac99a10b1bb35fd506b0d8
Oskar <oskarli@hotmail.se>
2023-12-10 20:36:03 +0100

    add swiper

---
+++ swiper.js
@@ -0,0 +1,25 @@
+// PRODUCT SLIDER
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    effect: 'fade',
+  
+    // // If we need pagination
+    // pagination: {
+    //   el: '.swiper-pagination',
+    // },
+  
+    // // Navigation arrows
+    // navigation: {
+    //   nextEl: '.swiper-arrow-next',
+    //   prevEl: '.swiper-arrow-prev',
+    // },
+  
+  });
+
+  
