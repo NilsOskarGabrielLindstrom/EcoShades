53156e2329427e7ffce58bdaa8f759d7e9a98d93
Oskar <oskarli@hotmail.se>
2023-12-11 04:13:49 +0100

    clean up

---
+++ js/swiper.js
@@ -0,0 +1,15 @@
+// PRODUCT SLIDER
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    speed: 2000,
+    effect: 'fade',
+  
+  });
+
+  
